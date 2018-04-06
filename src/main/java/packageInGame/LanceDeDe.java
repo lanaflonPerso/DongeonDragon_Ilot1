917b3482a94980314bf0cf12ef9b16e3612b5dea
Sébastien Marcoccia <sebastien.marcoccia@Digital-Grenoble.local>
2018-04-06 16:59:47 +0200

    Change lancer de dé

--- src/main/java/packageInGame/LanceDeDe.java
+++ src/main/java/packageInGame/LanceDeDe.java
@@ -19,7 +19,7 @@ public class LanceDeDe {
         boolean bouDuChemin = true;
         int plateauJeu[] = new int[64];
         int plateau = 50;
-        while (bouDuChemin) {
+        /*while (bouDuChemin) {
             try {
                 if (plateauJeu[plateau] < 64) {
                     System.out.println("<---------------------------------->");
@@ -44,6 +44,50 @@ public class LanceDeDe {
                 System.out.println("////////////////////////////////////////////////////////////////////////////" + "\n/////////////////////Vous étes au bout du chemin////////////////////////////" + "\n////////////////////////////////////////////////////////////////////////////");
                 bouDuChemin = false;
             }
+        }*/
+         
+        boolean loop = true;
+        boolean loop1 = true;
+        int test = 0;
+        
+        while(loop) {
+        	if(plateau < 64) {
+	            System.out.println("<---------------------------------->");
+	            System.out.println("Lancé le Dé en Appuyant sur entée");
+	            System.out.println("<---------------------------------->");
+	            sc.nextLine();
+	            System.out.println("<---------------------------------->");
+                test = deSix();
+                plateau += test;
+	            System.out.println("Votre position sur le plateau est : " + plateau + " <= 63 :");
+	            //plateauJeu[plateau] = plateau;
+        	}
+        	else 
+        		loop = false;
         }
+
+                while(loop1) {
+                		
+                		plateau -= test;
+		                System.out.println("<---------------------------------->");
+		                System.out.println("<---------Décrémentation----------->");
+		                System.out.println("<---------------------------------->");
+		                
+		                sc.nextLine();
+		                test = deSix();
+		                plateau += test;
+	                	
+	                	System.out.println("<-------------Lancer de Dé = " + test + "--------------------->");
+                //plateauJeu[plateau] = plateau;
+	                if(plateau > 64) {
+	                	System.out.println("if(" + plateau + " > 64 )");
+	                	loop1 = true;
+	                }
+	                else{
+	                		loop1 = false;
+	                		System.out.println("Valeur du plateau final : " + plateau);
+	                	}
+                }
+
     }
 }
