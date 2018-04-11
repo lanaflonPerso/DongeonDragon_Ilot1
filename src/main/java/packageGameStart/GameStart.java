69a80d548927ac753445a928e5ee7c9ffe8ee369
Sébastien Marcoccia <sebastien.marcoccia@Digital-Grenoble.local>
2018-04-11 13:59:53 +0200

    Suppression du choix du guerrier dans GameStart

--- src/main/java/packageGameStart/GameStart.java
+++ src/main/java/packageGameStart/GameStart.java
@@ -26,13 +26,13 @@ public class GameStart {
         System.out.println(MenuIntroduction.getDessinMenu());
         System.out.println("Que voulez vous faire :\n\t1-Jouer\n\t2-Quitter");
         String startGame = sc.nextLine();
+        
         if (startGame.equals("1")) {
             System.out.println("Nom du Player One");
             String playeOneName = sc.nextLine();
             AllList.playerNameList(playeOneName);
 
             System.out.println("<----------------------->");
-
 
             AllList.displayArrayList((AllList.playerNameList(playeOneName)));
             AllList.displayArrayList(AllList.dragonList());
@@ -44,48 +44,28 @@ public class GameStart {
 
             System.out.println("<----------------------->");
             boolean choixDePerso = true;
-            while (choixDePerso) {
-                System.out.println("Que voulez vous être :\n\t1-Guerrier\n\t2-Magicien :  ");
-                String choixPerso = sc.nextLine();
-                if (choixPerso.equals("1")) {
-                    // Factory.getInstanceGuerrier();
-                    System.out.println("vous etes un Guerrier");
-                    choixDePerso = false;
 
-                } else if (choixPerso.equals("2")) {
-                    System.out.println("vous etes un Magicien");
-                    choixDePerso = false;
+           ChoixPerso.choixPersonnage();
+
+
+            boolean lanceDe = true;
+
+            while (lanceDe) {
+                System.out.println("Que voulez vous faire :\n\t1-Commencer une Partie\n\t2-Sortir\n\t3-Quitter le jeu");
+                String choixDeSix = sc.nextLine();
+                if (choixDeSix.equals("1")) {
+                    LanceDeDe.plateauDeJeu();
+                } else if (choixDeSix.equals("2")) {
+                    lanceDe = false;
+                } else if (choixDeSix.equals("3")) {
+                    lanceDe = false;
+                    restInGame = false;
                 } else {
-                    System.out.println("<----------------------------------------->");
-                    System.out.println("Deux choix possible : 1-Guerrier 2-Magicien");
-                    System.out.println("<----------------------------------------->");
+                    System.out.println("<---------------------------------->");
+                    System.out.println("3 choix seulement : 1,2 ou,3");
+                    System.out.println("<---------------------------------->");
                 }
             }
-
-
-                System.out.println("<----------------------->");
-                
-               ChoixPerso.choixPersonnage();
-
-
-                boolean lanceDe = true;
-
-                while (lanceDe) {
-                    System.out.println("Que voulez vous faire :\n\t1-Commencer une Partie\n\t2-Sortir\n\t3-Quitter le jeu");
-                    String choixDeSix = sc.nextLine();
-                    if (choixDeSix.equals("1")) {
-                        LanceDeDe.plateauDeJeu();
-                    } else if (choixDeSix.equals("2")) {
-                        lanceDe = false;
-                    } else if (choixDeSix.equals("3")) {
-                        lanceDe = false;
-                        restInGame = false;
-                    } else {
-                        System.out.println("<---------------------------------->");
-                        System.out.println("3 choix seulement : 1,2 ou,3");
-                        System.out.println("<---------------------------------->");
-                    }
-                }
 
             } else if (startGame.equals("2")) {
                 restInGame = false;
