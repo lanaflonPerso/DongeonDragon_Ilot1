15190b4755eec9ec745b7f011fbe9ddec71bdda8
Radic4l <axel.dujardin@Digital-Grenoble.local>
2018-04-11 09:42:34 +0200

    local merge Dede

--- src/main/java/packageInGame/LanceDeDe.java
+++ src/main/java/packageInGame/LanceDeDe.java
@@ -19,32 +19,6 @@ public class LanceDeDe {
         boolean bouDuChemin = true;
         int plateauJeu[] = new int[64];
         int plateau = 50;
-        /*while (bouDuChemin) {
-            try {
-                if (plateauJeu[plateau] < 64) {
-                    System.out.println("<---------------------------------->");
-                    System.out.println("Lancé le Dé en Appuyant sur entée");
-                    System.out.println("<---------------------------------->");
-                    String lanceDe = sc.nextLine();
-                    System.out.println("<---------------------------------->");
-                    plateau += deSix();
-                    System.out.println("Votre position sur le plateau :" + plateau);
-                    plateauJeu[plateau] = plateau;
-
-                } else {
-                    System.out.println("<---------------------------------->");
-                    System.out.println("Lancé le Dé en Appuyant sur entée");
-                    System.out.println("<---------------------------------->");
-                    String lanceDe = sc.nextLine();
-                    System.out.println("<---------------------------------->");
-                    plateau += deSix();
-                    plateauJeu[plateau] = plateau;
-                }
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("////////////////////////////////////////////////////////////////////////////" + "\n/////////////////////Vous étes au bout du chemin////////////////////////////" + "\n////////////////////////////////////////////////////////////////////////////");
-                bouDuChemin = false;
-            }
-        }*/
          
         boolean loop = true;
         boolean loop1 = true;
@@ -91,3 +65,33 @@ public class LanceDeDe {
 
     }
 }
+
+///// Garbage
+
+// Boucle while with try and catch, if & else.
+/*while (bouDuChemin) {
+try {
+    if (plateauJeu[plateau] < 64) {
+        System.out.println("<---------------------------------->");
+        System.out.println("Lancé le Dé en Appuyant sur entée");
+        System.out.println("<---------------------------------->");
+        String lanceDe = sc.nextLine();
+        System.out.println("<---------------------------------->");
+        plateau += deSix();
+        System.out.println("Votre position sur le plateau :" + plateau);
+        plateauJeu[plateau] = plateau;
+
+    } else {
+        System.out.println("<---------------------------------->");
+        System.out.println("Lancé le Dé en Appuyant sur entée");
+        System.out.println("<---------------------------------->");
+        String lanceDe = sc.nextLine();
+        System.out.println("<---------------------------------->");
+        plateau += deSix();
+        plateauJeu[plateau] = plateau;
+    }
+} catch (ArrayIndexOutOfBoundsException e) {
+    System.out.println("////////////////////////////////////////////////////////////////////////////" + "\n/////////////////////Vous étes au bout du chemin////////////////////////////" + "\n////////////////////////////////////////////////////////////////////////////");
+    bouDuChemin = false;
+}
+}*/
