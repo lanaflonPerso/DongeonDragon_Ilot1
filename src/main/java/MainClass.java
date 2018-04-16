2e6b79277a7ac94096b26d0e0d03bf8040c19d17
Sébastien Marcoccia <sebastien.marcoccia@Digital-Grenoble.local>
2018-04-16 09:13:52 +0200

    modif main class

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,9 +1,34 @@
 import packageGameInit.GameBoard;
 import packageGameStart.*;
+import java.sql.*;
 
 public class MainClass {
     public static void main(String[] args) {
 //     	gameInit();
+        Connection conn=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/warriorjava","root","");
+        }catch(Exception e){
+            System.out.println("Echec !");
+            e.printStackTrace();
+        }
+        
+        if(conn!=null){
+            System.out.println("Succès!");
+        }
+        else{
+            System.out.println("Echec !");
+        }
+        
+        try {
+        	
+        	statement st=conn.createStatement();
+        	ResultSet resultat=st.executeQuery("select * from eleve");
+        	
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
     GameStart gameStart = new GameStart();
        gameStart.InGame();
 
---
+++ src/main/java/packageBdd/TestJDBC.java
@@ -0,0 +1,17 @@
+/*package packageBdd;
+
+import java.util.ArrayList;
+import java.util.List;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
+// import java.sql.Connection;
+import java.sql.*;
+
+
+public class TestJDBC{
+    private static String URL = "jdbc:mysql://localhost:3306/warriorjava";
+    private static String LOGIN = "root";
+    private static String PASSWORD = "";
+
+
+} */
